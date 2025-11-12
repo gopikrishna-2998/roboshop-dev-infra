7d8dfae0f7b2235ea9ec0d50c609290404ed6ed6
gopikrishna <gopikrishnamaneem@gmail.com>
2025-11-12 10:29:22 +0530

    terraform

---
+++ 00-vpc/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+    source = "git::https://github.com/gopikrishna-2998/terraform-aws-vpc.git"
+    vpc_cidr = var.vpc_cidr
+    project_name = var.project_name
+    environment = var.environment
+    vpc_tags = var.vpc_tags
+
+    #public_subnet
+    public_subnet_cidrs = var.public_subnet_cidrs
+     #private_subnet
+    private_subnet_cidrs = var.private_subnet_cidrs
+     # database subnets
+    database_subnet_cidrs = var.database_subnet_cidrs
+
+    is_peering_required = true
+}
+
